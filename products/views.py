bc4f1d027a7a3b496b1fd662d4253e1238e2e545
bridcourtney <brid.courtney@gmail.com>
2021-02-04 16:50:06 +0000

    created course & course detail views, urls & templates

--- products/views.py
+++ products/views.py
@@ -6,14 +6,13 @@ from .models import Product
 def all_products(request):
     """ A view to show all products, including sorting and search queries """
 
-    products = Product.objects.all()
+    products = Product.objects.filter(is_a_service=False)
 
     context = {
         'products': products,
     }
 
     return render(request, 'products/products.html', context)
-
 
 def product_detail(request, product_id):
     """ A view to show individual product details """
@@ -25,3 +24,25 @@ def product_detail(request, product_id):
     }
 
     return render(request, 'products/product_detail.html', context)
+
+
+def courses(request):
+    """ A view to display all of the courses"""
+    products = Product.objects.filter(is_a_service=True)
+    context = {
+        'products': products,
+    }
+
+    return render(request, 'products/courses.html', context)
+
+
+def course_detail(request, product_id):
+    """ A view to show individual course details """
+
+    product = get_object_or_404(Product, pk=product_id)
+
+    context = {
+        'product': product,
+    }
+
+    return render(request, 'products/course_detail.html', context)
