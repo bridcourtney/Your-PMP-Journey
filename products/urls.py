f34df0e8dd20b46871c5120b214ca5c203966f61
bridcourtney <brid.courtney@gmail.com>
2021-02-11 14:32:32 +0000

    added delete option to product admin

--- products/urls.py
+++ products/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('course_detail/<product_id>', views.course_detail, name='course_detail'),
     path('add/', views.add_product, name='add_product'),
     path('edit/<int:product_id>/', views.edit_product, name='edit_product'),
+    path('delete/<int:product_id>/', views.delete_product, name='delete_product'),
 ]
