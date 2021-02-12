4db176fae786161d46a15747b2b19910a527b9fc
bridcourtney <brid.courtney@gmail.com>
2021-02-12 17:39:12 +0000

    removed secret key and set debug

--- your_pmp_journey/settings.py
+++ your_pmp_journey/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '^edn_ic*oy_6^vf(@*e0l@sj2n%ky042#i+*0!=^p-l^5li$ca'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['pmp-journey.herokuapp.com', 'localhost']
 
