5be107b164048c4955f51edd7b1b93827543a7f5
bridcourtney <brid.courtney@gmail.com>
2021-02-12 17:08:34 +0000

    deployment to postgres

--- products/forms.py
+++ products/forms.py
@@ -3,6 +3,8 @@ from .widgets import CustomClearableFileInput
 from .models import Product, Category
 
 
+
+
 class ProductForm(forms.ModelForm):
 
     class Meta:
--- products/models.py
+++ products/models.py
@@ -1,5 +1,4 @@
 from django.db import models
-from django.core.validators import MinValueValidator, MaxValueValidator
 
 
 class Category(models.Model):
@@ -39,10 +38,6 @@ class Product(models.Model):
     rating = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
     image_url = models.URLField(max_length=1024, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
-    duration = models.IntegerField(null=True, blank=True,
-                                   validators=[MinValueValidator(1),
-                                               MaxValueValidator(100)])
-   
 
     def __str__(self):
         return self.name
