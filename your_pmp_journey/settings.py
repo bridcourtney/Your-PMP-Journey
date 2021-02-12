2ff6fe41662e229c9da598f543a33e4a5450025a
bridcourtney <brid.courtney@gmail.com>
2021-02-12 22:08:56 +0000

    testing build to heroku

--- your_pmp_journey/settings.py
+++ your_pmp_journey/settings.py
@@ -174,11 +174,13 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 if 'USE_AWS' in os.environ:
     # Bucket Config
+    AWS_DEFAULT_ACL = None
     AWS_STORAGE_BUCKET_NAME = 'pmp-journey'
     AWS_S3_REGION_NAME = 'eu-west-1'
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
     AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
     AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
+    
 
     # Static and media files
     STATICFILES_STORAGE = 'custom_storages.StaticStorage'
