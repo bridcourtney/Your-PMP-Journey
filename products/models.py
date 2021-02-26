9ea6dec3acf479b6bb3c1b8ef41983da6f678606
bridcourtney <brid.courtney@gmail.com>
2021-02-26 14:57:40 +0000

    styled course detail template.  add video flag to product model

--- products/models.py
+++ products/models.py
@@ -34,6 +34,7 @@ class Product(models.Model):
     author = models.CharField(max_length=254)
     description = models.TextField()
     is_a_service = models.BooleanField(default=False)
+    is_a_video = models.BooleanField(default=False)
     price = models.DecimalField(max_digits=6, decimal_places=2)
     image_url = models.URLField(max_length=1024, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
